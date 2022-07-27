d426e78fa0ed26307732301b894f81da5d8c2b9c
Shape Lee <shape1026@gmail.com>
2022-07-28 00:51:59 +0900

    [220727] touch slide 수정 중

--- js/slide.js
+++ js/slide.js
@@ -3,45 +3,113 @@ const next = document.querySelector(".work_next");
 const slideWrap = document.querySelector('.work_slide');
 const liList = slideWrap.querySelectorAll("li");
 let liIndex = liList.length;
-let positionValue = slideWrap.getAttribute("data-position");
+let dataPosition = slideWrap.getAttribute("data-position");
 
 // console.log(liList[0].clientWidth)
 
 const transNext = (e) => {
   e.preventDefault();
   
-  positionValue = Number(positionValue) - 100;
+  dataPosition = Number(dataPosition) - 100;
 
   // if(positionValue === -(liIndex - 1) * 100) {
   //   next.removeEventListener('click', transNext);
   //   next.classList.remove("work_next-hover");
   // }
   
-  console.log(positionValue)
+  console.log(dataPosition)
   console.log(-(liIndex - 1) * 100)
-  slideWrap.style.transform = "translateX(" + positionValue + "%)"
+  slideWrap.style.transform = "translateX(" + dataPosition + "%)"
   slideWrap.style.transition = "transform 1s"
-  slideWrap.setAttribute("data-position", positionValue)
+  slideWrap.setAttribute("data-position", dataPosition)
 
-  if(Number(positionValue) < 0) {
+  if(Number(dataPosition) < 0) {
     prev.addEventListener('click', transPrev);
     prev.classList.add("work_prev-hover");
+  }
+
+  if(Number(dataPosition) === -(liIndex - 1) * 100) {
+    next.removeEventListener('click', transNext);
+    next.classList.remove("work_next-hover");
   }
 }
 
 const transPrev = (e) => {
-  positionValue = Number(positionValue) + 100;
+  dataPosition = Number(dataPosition) + 100;
 
-  slideWrap.style.transform = "translateX(" + positionValue + "%)"
+  slideWrap.style.transform = "translateX(" + dataPosition + "%)"
   slideWrap.style.transition = "transform 1s"
-  slideWrap.setAttribute("data-position", positionValue)
+  slideWrap.setAttribute("data-position", dataPosition)
 
-  if(Number(positionValue) === 0) {
+  next.addEventListener('click', transNext);
+  next.classList.add("work_next-hover");
+
+  if(Number(dataPosition) === 0) {
     prev.removeEventListener('click', transPrev);
     prev.classList.remove("work_prev-hover");
   }
 }
 
+
+if(slideWrap.clientWidth < liIndex * liList[0].clientWidth) {
+  next.addEventListener('click', transNext);
+  next.classList.add("work_next-hover");
+} 
+/*----------------------------------------------*/
+let touchstartX = 0;
+let mouseStart = false;
+let currentTarget = "";
+let currentActiveLi;
+let nowAcriveLi;
+let currentClassList;
+
+
+const processTouchEnd = (e) => {
+  if(mouseStart === true) {
+    currentTarget.removeEventListener('mousemove', processTouchMove);
+    currentTarget.removeEventListener('mouseup', processTouchEnd);
+
+    currentTarget.removeEventListener('touchmove', processTouchMove);
+    currentTarget.removeEventListener('touchend', processTouchEnd);  
+
+  }
+}
+
+const processTouchMove = (e) => {
+  e.preventDefault();
+
+  let currentX = e.clientX || e.touches[0].screenX;
+  nowAcriveLi = (Number(currentX) - Number(touchstartX));
+  console.log(nowAcriveLi, currentActiveLi, currentX, touchstartX)
+
+  slideWrap.style.transition = 'transform 0s linear';
+  slideWrap.style.transform = "translateX(" + nowAcriveLi + "px)"
+}
+
+const processTouchStart = (e) => {
+  mouseStart = true;
+
+  e.preventDefault()
+
+  touchstartX = e.clientX || e.touches[0].screenX;
+  currentTarget = e.target
+
+  currentTarget.addEventListener('mousemove', processTouchMove);
+  currentTarget.addEventListener('mouseup', processTouchEnd);
+
+  currentTarget.addEventListener('touchmove', processTouchMove);
+  currentTarget.addEventListener('touchend', processTouchEnd);  
+
+  currentActiveLi = dataPosition;
+}
+
+liList.forEach( item => {
+  item.addEventListener("mousedown", processTouchStart);
+  item.addEventListener("touchstart", processTouchStart);
+});
+
+window.addEventListener('dragend', processTouchEnd);
+window.addEventListener('mouseup', processTouchEnd);
 // if(Number(positionValue) !== 0) {
 //   prev.addEventListener('click', transPrev);
 //   prev.classList.add("work_prev-hover");
@@ -49,11 +117,6 @@ const transPrev = (e) => {
 //   prev.removeEventListener('click', transPrev);
 //   prev.classList.remove("work_prev-hover");
 // }
-
-if(slideWrap.clientWidth < liIndex * liList[0].clientWidth) {
-  next.addEventListener('click', transNext);
-  next.classList.add("work_next-hover");
-} 
 
 // else if (positionValue === -(liIndex - 1) * 100) {
 //   next.removeEventListener('click', transNext);
