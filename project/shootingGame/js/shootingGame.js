6226c9bbe82ecd2efb766792ad7fd0ea1094d458
Shape Lee <shape1026@gmail.com>
2023-01-31 15:03:16 +0900

    Markup: JS 슈팅게임 프로젝트 추가

--- project/shootingGame/js/shootingGame.js
+++ project/shootingGame/js/shootingGame.js
@@ -15,27 +15,16 @@ function createImage() {
   const imgBox = document.querySelector(".site-img_box");
   const imgList = [
     {
-      src: "./images/minigame_rsp_m.gif",
-      alt: "Rock Scissers Paper Game mobile",
-    },
-    {
-      src: "./images/minigame_rsp.gif",
-      alt: "Rock Scissers Paper Game",
+      src: "./images/shooting_game.gif",
+      alt: "JS Shooting Game - Space2023",
     },
   ];
   const imgTag = document.createElement("img");
 
   imgTag.setAttribute("loading", "lazy");
-
-  if (window.matchMedia("(min-width: 768px)").matches) {
-    imgTag.setAttribute("src", imgList[1].src);
-    imgTag.setAttribute("alt", imgList[1].alt);
-    imgBox.appendChild(imgTag);
-  } else {
-    imgTag.setAttribute("src", imgList[0].src);
-    imgTag.setAttribute("alt", imgList[0].alt);
-    imgBox.appendChild(imgTag);
-  }
+  imgTag.setAttribute("src", imgList[0].src);
+  imgTag.setAttribute("alt", imgList[0].alt);
+  imgBox.appendChild(imgTag);
 }
 
 window.addEventListener("load", createImage);
